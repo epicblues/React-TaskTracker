2082aa1358dcf28ce45d419156de764661bc80be
KMS <epicblue@hanmail.net>
2021-09-22 15:40:53 +0900

    task tracker with json-server API PATCH POST

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
-
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import {useState} from 'react';
-import AddForm from './components/AddForm';
-
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddForm from "./components/AddForm";
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    "id" : 1,
-    "text" : "Doctors Appointment",
-    "day": "March 1st at 2:30pm",
-    "reminder": true
-  },
-  {
-    "id" : 2,
-    "text" : "Meeting At School",
-    "day":"March 3rd at 1:30pm",
-    "reminder": true
-  },
-  {
-    "id" : 3,
-    "text" : "Food Shopping",
-    "day": "March 3st at 11:30am",
-    "reminder": false
-  }]);
-  const [display, setDisplay] = useState(false)
+  const [tasks, setTasks] = useState([]);
+  const [display, setDisplay] = useState(false);
 
-  
+  async function fetchDbAndSetTasks() {
+    const response = await fetch("http://localhost:3000/tasks");
+    const json = await response.json();
+    setTasks(json);
+  }
+  if (tasks.length === 0) {
+    fetchDbAndSetTasks();
+  }
 
   const toggleReminder = (id) => {
-    setTasks([...tasks].map(task => task.id === id 
-      ? {...task, reminder:!task.reminder} 
-      : task))
-  }
+    const toggledTask = tasks.find((task) => task.id === id);
+    toggledTask.reminder = !toggledTask.reminder;
+    fetch("http://localhost:3000/tasks/" + id, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(toggledTask),
+    })
+      .then((res) => res.json())
+      .then(fetchDbAndSetTasks);
+  };
   const deleteTask = (id) => {
-    setTasks([...tasks].filter(task => task.id !== id));
-  }
+    fetch("http://localhost:3000/tasks/" + id, {
+      method: "DELETE",
+    }).then(fetchDbAndSetTasks);
+  };
 
   const addTask = (task) => {
-    
-    const id = Math.floor(Math.random() * 1000000);
-    const newTask = {id, ...task};
-    setTasks([...tasks,newTask]);
-  }
-
-  
+    console.log(task);
+    fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    }).then(fetchDbAndSetTasks);
+  };
 
   return (
-    <div className="container"> 
-      <Header title="Task Tracker" onToggle={(e) => setDisplay(!display)} display={display}/>
-      {display && <AddForm onAdd={addTask}/>}
-      {tasks.length !== 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : <h3>No Tasks To Show</h3>}
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onToggle={(e) => setDisplay(!display)}
+        display={display}
+      />
+      {display && <AddForm onAdd={addTask} />}
+      {tasks.length !== 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        <h3>No Tasks To Show</h3>
+      )}
     </div>
-    
   );
 }
-
-
 
 export default App;
